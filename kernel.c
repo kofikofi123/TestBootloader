0f51df4302a2fad8ac609e1bb5a0688ae2850270
kofikofi123 <usingnamespaceasd@gmail.com>
2016-02-15 17:38:41 +0000

    Commited from koding.com

--- kernel.c
+++ kernel.c
@@ -1,7 +1,7 @@
 #include "include/Sys.h"
 
 int kernel_main(void){
-	__asm__("pushad");
+	//__asm__("pushad");
 	init_gdt();
 	init_idt();
 	init_pic();
@@ -11,7 +11,7 @@ int kernel_main(void){
 	
 	os_sti();
 	
-	__asm__("popad");
+	//__asm__("popad");
 	//kernel_sleep()
 	//sort_mapped_memory();
 	
