c59b7d7387a5a7284b825c0d9ab3f27658e8463d
Jonathan Cooper <drowsysaturn@gmail.com>
2016-05-13 18:52:14 -0400

    Updated string tools

--- include/String.h
+++ include/String.h
@@ -1,5 +1,5 @@
-//String.h, will possibly move it somewhere else 
-
+// The goal of this file is to provide high-performance string manipulation
+// tools.
 
 #ifndef _STRING_H
 #define _STRING_H
@@ -9,7 +9,41 @@
 #define STRING_END -1
 #define STRING_NOMATCH -2
 
-int32_t string_find(const char*, const char*, uint32_t, uint32_t);
-uint32_t string_len(const char*);
+/**
+ * Search for a substring within a range.
+ * @param source The source string
+ * @param substring The sub string to find
+ * @param start The beginning of the source to search
+ * @param finish The end of the source to search or STRING_END
+ * @see STRING_END
+ * @see STRING_NOMATCH
+ * @return The location that substring was first found or STRING_NOMATCH
+ */
+int32_t string_find_range(const char *source, const char *substring,
+        intptr_t start, intptr_t finish);
+
+/**
+ * Search for a substring within a source string
+ * @param source The source string
+ * @param substring The substring to find
+ * @see STRING_NOMATCH
+ * @return The location that substring was first found or STRING_NOMATCH
+ */
+int32_t string_find(const char *source, const char *substr);
+
+/**
+ * Compares two strings
+ * @param a The first string
+ * @param b The second string
+ * @return 1 if the strings were the same, otherwise 0.
+ */
+int32_t string_compare(const char *a, const char *b);
+
+/**
+ * Gets the length of a source string
+ * @param source The string to get the length of
+ * @return The length of source
+ */
+uint32_t string_len(const char *source);
 
 #endif
--- src/String.c
+++ src/String.c
@@ -1,36 +1,81 @@
-#include <stdint.h>
 #include "../include/String.h"
 
-
-int32_t string_find(const char* str, const char* cocstr, uint32_t start, uint32_t finish){
-	const uint32_t size = string_len(str);
-	//const uint32_t csize = string_len(cocstr);
-	
-	int32_t temp = STRING_NOMATCH;
-	if (finish == STRING_END || finish > size)
-		finish = size;
-	
-	
-	
-	char* ptr = (char*)(str + start);
-	char* ptr2 = (char*)(cocstr);
-	
-	while (start < finish && (*ptr != 0 || *ptr2 != 0)) {
-		if (*ptr == *ptr2){
-			if (temp == STRING_NOMATCH)
-				temp = start;
-		}else{
-			temp = STRING_NOMATCH;
-		}
-		start++;
-	}
-	
-	return temp;
+int32_t string_find_range(const char *source, const char *substr,
+	intptr_t start, intptr_t finish)
+{
+    if (finish == STRING_END)
+    {
+        finish = INTPTR_MAX;
+    }
+    else
+    {
+        finish += (intptr_t)source;
+    }
+    char *caSource = (char*)(source+start), *caSubstr = (char*)substr;
+    char marker;
+    char subFirstLetter = *caSubstr;
+    while ((marker = *caSource) != 0 && (intptr_t)caSource <= finish)
+    {
+        if (marker == subFirstLetter)
+        {
+            int i;
+            for (i = 0; caSubstr[i] != 0 && caSource[i] != 0; ++i)
+            {
+                if (caSubstr[i] != caSource[i])
+                {
+                    break;
+                }
+            }
+            if (caSubstr[i] == 0)
+            {
+                if ((intptr_t)caSource <= finish)
+                {
+                    return (int32_t)(caSource-source);
+                }
+            }
+        }
+        ++caSource;
+    }
+    return STRING_NOMATCH;
 }
 
+int32_t string_compare(const char *a, const char *b)
+{
+    char *caA = (char*)a, *caB = (char*)b;
+    while (*caA != 0 && *caA++ == *caB++);
+    return (*caA == 0 && *caB == 0) ? 1 : 0;
+}
 
-uint32_t string_len(const char* str){
-	char* temp = (char*)str; 
+int32_t string_find(const char *source, const char *substr)
+{
+    char *caSource = (char*)source, *caSubstr = (char*)substr;
+    char marker;
+    char subFirstLetter = *caSubstr;
+    while ((marker = *caSource) != 0)
+    {
+        if (marker == subFirstLetter)
+        {
+            int i;
+            for (i = 1; caSubstr[i] != 0 && caSource[i] != 0; ++i)
+            {
+                if (caSubstr[i] != caSource[i])
+                {
+                    break;
+                }
+            }
+            if (caSubstr[i] == 0)
+            {
+                return caSource-source;
+            }
+        }
+        ++caSource;
+    }
+    return STRING_NOMATCH;
+}
+
+uint32_t string_len(const char* str)
+{
+	char* temp = (char*)str;
 	while (*temp != 0){
 		++temp;
 	}
