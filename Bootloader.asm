edf13d7afc6112883f066c00defa730d0b3e1023
xxx_triangle_xxx the chair <ofosu09@gmail.com>
2016-11-22 04:33:02 +0000

    New

---
+++ Bootloader.asm
@@ -0,0 +1,54 @@
+org 0x7C00
+
+jmp 0x00:Main
+
+Reset_drive:
+    mov ax, 0
+    int 0x13 
+    lahf 
+    and ax, 1 
+    ret
+    
+Print_string: ;we are going to use the interrupt service because I do not need to directly write to it right now
+    pusha
+    pushf 
+    cld
+    
+.looper:
+    mov al, [edi]
+    
+    or al, al 
+    jz .fini 
+    
+    mov bh, 0 
+    mov cx, 1 
+    int 0x10 
+.fini:
+    popf
+    popa 
+    ret
+
+Main:
+    xor ax, ax 
+    mov ds, ax
+    
+    cli
+    mov ss, ax
+    mov sp, 0x7DFF
+    sti 
+    
+    mov cl, 5
+.clear_drive_begin:
+    call Reset_drive 
+    or ax, 1 
+    jnz .start_parsing
+    loop .clear_drive_begin 
+.start_parsing:
+    
+looper:
+    jmp looper
+    
+;variables
+bdrive: db 0
+error_message: db "Unable to reset boot drive."
+times 512-($$-$) db 0
---
+++ README.md
@@ -0,0 +1,2 @@
+# NewStickOS 3.0<br/>
+I have learned enough mistakes (hopefully), and decided to rewrite again.
